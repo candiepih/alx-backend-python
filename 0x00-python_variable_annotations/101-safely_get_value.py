2828b1830fc2353804b0d7b7682460f341712b4c
Candiepih <mutheeal.am@gmail.com>
2021-12-02 13:30:54 +0300

    Formatted file to pep8 linting

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,7 +5,8 @@ from typing import Mapping, Any, Union, TypeVar, Optional
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Optional[T] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
     """Safely gets value from dictionary.
 
     Args:
